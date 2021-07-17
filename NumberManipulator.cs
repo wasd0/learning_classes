af67a7c6aed29d5c30d486f31d984d0d2bdcaccb
Shvedov Kirill <kirshvedov@bk.ru>
2021-07-17 13:57:10 +0300

    Добавьте файлы проекта.

---
+++ NumberManipulator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_classes
+{
+    class NumberManipulator
+    {
+       public int FindMax(int first, int second)
+        {
+            int result; //Здесь хранится наибольшее число
+
+            if (first > second)
+            {
+                result = first;
+            }
+            else
+            {
+                result = second;
+            }
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result;
+            NumberManipulator numberManipulator = new NumberManipulator();
+
+            result = numberManipulator.FindMax(50, 30);
+
+            Console.WriteLine(result + " Мама Владика даёт.");
+            if (DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("Ну и чего ты встал так рано? А ну дуй в постель!");
+            }
+            else
+            {
+                Console.WriteLine("Ого, а кто это у нас так долго спит? Мог бы и не вставать уже!");
+            }
+            Console.WriteLine("Выбери себе прозвище, боец!");
+            string player_name = Console.ReadLine();
+            Console.WriteLine("Отлично! {0}, что бы ты хотел отведать сегодня?", player_name);
+            string order = Console.ReadLine();
+            Console.WriteLine("К сожалению, мы не сможем дать тебе {0}. Зато дадим тебе горячих пиздюлин!", order);
+            int time = new Random().Next(5, 20);
+            Console.WriteLine("У тебя {0} минут, чтобы съебать отсюда. Время пошло.", time);
+            Console.ReadKey();
+        }
+
+
+    }
+}
